5005911ef8a5055f3c49ecf8b4b430cf12167fd4
hopenz <petrushina_03@inbox.ru>
2024-10-24 16:16:02 +0500

    Initial commit

---
+++ src/main/java/ru/naumen/naumen_schooldairy/NaumenSchoolDairyApplication.java
@@ -0,0 +1,13 @@
+package ru.naumen.naumen_schooldairy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NaumenSchoolDairyApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(NaumenSchoolDairyApplication.class, args);
+    }
+
+}
---
+++ src/test/java/ru/naumen/naumen_schooldairy/NaumenSchoolDairyApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.naumen.naumen_schooldairy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NaumenSchoolDairyApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
